b8b4fd71eb5a30650af3b8ec1a9f010eefe7d643
hiroyoyo <k014c1139@it-neec.jp>
2017-09-06 10:25:27 +0900

    新しいiphoneの画面

---
+++ contactbookTests/contactbookTests.swift
@@ -0,0 +1,36 @@
+//
+//  contactbookTests.swift
+//  contactbookTests
+//
+//  Created by 佐野浩代 on 2017/09/06.
+//  Copyright © 2017年 佐野浩代. All rights reserved.
+//
+
+import XCTest
+@testable import contactbook
+
+class contactbookTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
