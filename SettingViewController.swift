d0e1d0f1cde3b16154eac1308896fe1b3ac71002
hiroyoyo <k014c1139@it-neec.jp>
2017-09-19 09:33:11 +0900

    no message

--- SettingViewController.swift
+++ SettingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SettingViewController.swift
 //  contactbook
 //
 //  Created by 佐野浩代 on 2017/09/06.
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SettingViewController : UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
---
+++ contactbook/HomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HomeViewController.swift
+//  contactbook
+//
+//  Created by 佐野浩代 on 2017/09/06.
+//  Copyright © 2017年 佐野浩代. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+    }
+    @IBAction func button(_ sender: Any) {
+         }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
